c940595383006c768f515e68cd2910a29c46607b
2uiwnsx <2uiwnsx@gmail.com>
2024-01-13 20:58:24 +0900

    Add: ex29_modeling.sql 파일 추가

---
+++ ex29_modeling.sql
@@ -0,0 +1,33 @@
+/*
+    ▶ 데이터베이스 설계 단계
+
+        ▷ 개념 모델링
+        ▷ 논리 모델링
+        ▷ 물리 모델링
+        
+    ▶ ERD(Entity-Relationship Diagram)
+    
+        ▷ 개체(Entity) : 직사각형
+        ▷ 속성(Attribute) : 타원
+        ▷ 관계(Relationship) : 마름모
+        
+    ▶ 이상 현상(Anomaly)
+    
+        ▷ 삽입 이상(Insertion Anomaly)
+        ▷ 삭제 이상(Deletion Anomaly)
+        ▷ 갱신 이상(Update Anomaly)
+        
+    ▶ 함수 종속(Functional Dependency)
+        
+        ▷ 부분 함수 종속
+        ▷ 완전 함수 종속
+        ▷ 이행 함수 종속
+    
+    ▶ 정규화(Normalization)
+    
+        ▷ 1정규형(1NF) : 원자값으로 구성
+        ▷ 2정규형(2NF) : 부분 함수 종속 제거
+        ▷ 3정규형(3NF) : 이행 함수 종속 제거
+    
+    ▶ 반 정규화(De-Normalization)
+*/
